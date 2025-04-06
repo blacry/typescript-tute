1a42029544102073b100f269ce2e12e7399a4b29
Vivek <77609053+blacry@users.noreply.github.com>
2025-04-06 21:57:56 +0000

    thats the end of the video atleast the free one

--- typescript-tute/dist/index.js
+++ typescript-tute/dist/index.js
@@ -13,5 +13,52 @@ function render(doc) {
     });
     let tuples = [1, 'one'];
     tuples.push(50);
+    let dict = [tuples];
+    dict = [[1, 'one'], [2, 'two']];
+    console.log(dict);
 }
+console.log(0, 1, 2);
+var Sizes;
+(function (Sizes) {
+    Sizes[Sizes["s"] = 5] = "s";
+    Sizes[Sizes["m"] = 6] = "m";
+    Sizes[Sizes["l"] = 7] = "l";
+})(Sizes || (Sizes = {}));
+console.log(Sizes.s, Sizes.m, Sizes.l);
+let employee;
+employee = {
+    id: 1, retire: (date) => {
+        console.log(date);
+    }
+};
+employee.name = 'Mosh';
+let employees;
+function kgtolbs(weight) {
+    if (typeof weight === 'number') {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
+let textbox = {
+    drag: () => { },
+    resize: () => { }
+};
+let distance;
+distance = 'cm';
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase() + ' hello');
+    else
+        console.log('fuck you');
+}
+greet(null);
+greet(undefined);
+greet('shjad');
+function getCustomer(id) {
+    return id === 0 ? null : { bday: new Date() };
+}
+let costomer1 = getCustomer(0);
+let costomer2 = getCustomer(5);
 //# sourceMappingURL=index.js.map
--- typescript-tute/src/index.ts
+++ typescript-tute/src/index.ts
@@ -8,6 +8,7 @@ lvl = '1'
 
 function render(doc: any) {
     console.log(doc);
+
     // arreys
     let numbers: number[] = []
     numbers.forEach(e => {
@@ -16,11 +17,103 @@ function render(doc: any) {
 
     // tuples /** not same as python more like a pair of key value and thats it**/
 
-    let tuples: [number,string] = [1,'one']
+    let tuples: [number, string] = [1, 'one']
     tuples.push(50) // this works for some reason but it shouldn't
     // we can make dictionary of this tuple
     let dict: object[] = [tuples]
-    dict =[[1,'one'],[2,'two']]
+    dict = [[1, 'one'], [2, 'two']]
+    console.log(dict);
+
 
 }
 
+// now enum here, we use related constants and define them together
+const enum Size { s, m, l }
+console.log(Size.s, Size.m, Size.l);
+enum Sizes { s = 5, m, l }
+console.log(Sizes.s, Sizes.m, Sizes.l);
+
+// fuctions me bhi ham type define kar sakte hai both for parameter and fxn itself and we can define the type for what the fxn will return too and also how many parameter can be passed while calling the fxn
+
+// now bout object or dictionary
+
+// here u can see mene name intialise kiya hai lekin object me hai ni still it works as i have added that ? mark after name which makes it and optional requirment
+// / readonly makes us that we cant change the value but only able to call it and print it and use it but cant modify it
+
+let employee: {
+    readonly id: number, name?: string, retire: (date: Date) => void
+}
+employee = {
+    id: 1, retire: (date: Date) => {
+        console.log(date);
+    }
+}
+employee.name = 'Mosh'
+
+//type alias
+
+type Employee = {
+    readonly id: number, name?: string, retire: (date: Date) => void
+}
+// now we can do 
+let employees: Employee
+
+// union types
+
+function kgtolbs(weight: number | string): number {
+    if (typeof weight === 'number') {
+        return weight * 2.2
+    }
+    else {
+        return parseInt(weight) * 2.2
+    }
+}
+
+// intersection types
+
+type Draggable = {
+    drag: () => void
+}
+type Resizable = {
+    resize: () => void
+}
+type UIwidgit = Draggable & Resizable
+
+let textbox: UIwidgit = {
+    drag: () => { },
+    resize: () => { }
+}
+
+// litral type
+let distance: Distance 
+type Distance = 'cm' | "m" | 'km'
+distance = 'cm'
+
+//Nullable type
+
+function greet(name:string |null |undefined) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log('fuck you');    
+}
+greet(null)
+greet(undefined)
+greet('shjad')
+
+// optional chaining
+
+type Customer = {
+    bday: Date
+};
+
+function getCustomer(id:number):Customer | null  {
+    return id === 0 ? null : {bday : new Date()};
+}
+let costomer1 = getCustomer(0)
+let costomer2 = getCustomer(5)
+console.log(costomer1?.bday, costomer2?.bday.getFullYear());
+
+//optional element access operator
+let log: any = null
+log?.('a')
